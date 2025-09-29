a9e768b38bdf99dc6f91f22357f860c162eeb608
sandrea13 <s.andrea@correo.unimet.edu.ve>
2025-09-29 12:28:02 -0400

    Añadir funciones principales del juego y comentarios
    Falta todavía por implementar la clase AhorcadoPainter

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class AhorcadoPage extends StatefulWidget {
   AhorcadoPageState createState() => AhorcadoPageState();
 }
 
+// Estado de la página del Ahorcado
 class AhorcadoPageState extends State<AhorcadoPage> {
   List<String> palabras = ['INTERNET', 'INFORMACION', 'JUEGO', 'CODIGO', 'MOVIL', 'PROGRAMA', 'COMPUTADORA'];
   String palabra = '';
@@ -46,5 +47,63 @@ class AhorcadoPageState extends State<AhorcadoPage> {
       errores = 0;
       dialogoMostrado = false; // Resetear al empezar nuevo juego
     });
+  }
+
+  void adivinarLetra(String letra) {
+    // Retornar si se adivinó una letra
+    if (letrasUsadas.contains(letra)) return;
+    // De lo contrario, actualizar errores o terminar
+    setState(() {
+      // Incrementar los errores si se cometió uno
+      letrasUsadas.add(letra);
+      if (!palabra.contains(letra)) {
+        errores++;
+      }
+      // Mostrar mensaje de fin si el juego terminó
+      if ((gano() || perdio()) && !dialogoMostrado) {
+        partidasJugadas++;
+        dialogoMostrado = true;
+      }
+    });
+  }
+
+  // Mostrar la palabra mientras se va adivinando
+  String mostrarPalabra() {
+    String resultado = '';
+    for (int i = 0; i < palabra.length; i++) {
+      String letra = palabra[i];
+      if (letrasUsadas.contains(letra)) {
+        resultado += '$letra ';
+      } else {
+        resultado += '_ ';
+      }
+    }
+    return resultado;
+  }
+
+  // Función para evaluar si el usuario ganó
+  bool gano() {
+    for (int i = 0; i < palabra.length; i++) {
+      if (!letrasUsadas.contains(palabra[i])) {
+        return false; // No ha ganado si una letra no es correcta
+      }
+    }
+    return true;
+  }
+
+  // Función para evaluar si el usuario perdió
+  bool perdio() {
+    return errores >= maxErrores; // Pierde si los errores superan el máximo
+  }
+
+  // Contenedor del dibujo principal
+  Widget dibujarAhorcado() {
+    return Container(
+      height: 200,
+      width: 150,
+      child: CustomPaint(
+        painter: AhorcadoPainter(errores),
+      ),
+    );
   }
 }
